5e3d348e00f44fe13fe6d680de8aa38169528ed1
tlzm <50614481+tlzm@users.noreply.github.com>
2021-05-17 09:21:38 +0800

    Add files via upload

---
+++ featuremethod/dataprocess.py
@@ -0,0 +1,234 @@
+from docutils.parsers.rst.directives import positive_int
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import MinMaxScaler,StandardScaler,MaxAbsScaler
+
+learningbearings = ['Bearing1_1','Bearing1_2','Bearing2_1','Bearing2_2','Bearing3_1','Bearing3_2']
+testingbearings = ['Bearing1_3','Bearing1_4','Bearing1_5','Bearing1_6','Bearing1_7','Bearing2_3','Bearing2_4','Bearing2_5','Bearing2_6','Bearing2_7','Bearing3_3']
+finalRUL = [573,290,161,146,757,753,139,309,129,58,82]
+
+for bearing in learningbearings:
+    df_td = pd.read_csv('input/'+bearing + '_time_domain_features.csv')
+    df_fd = pd.read_csv('input/'+bearing + '_freq_domain_features.csv')
+    df_tfd = pd.read_csv('input/'+bearing + '_timefreq_domain_features.csv')
+    df = pd.DataFrame({'time': df_td['time'],
+                       'f1': df_td['haccel_mean'],
+                       'f2': df_td['vaccel_mean'],
+                       'f3': df_td['haccel_max'],
+                       'f4': df_td['vaccel_max'],
+                       'f5': df_td['haccel_min'],
+                       'f6': df_td['vaccel_min'],
+                       'f7': df_td['haccel_p2p'],
+                       'f8': df_td['vaccel_p2p'],
+                       'f9': df_td['haccel_var'],
+                       'f10': df_td['vaccel_var'],
+                       'f11': df_td['haccel_kurt'],
+                       'f12': df_td['vaccel_kurt'],
+                       'f13': df_td['haccel_skew'],
+                       'f14': df_td['vaccel_skew'],
+
+                       'f15': df_fd['haccel_freq_means'],
+                       'f16': df_fd['vaccel_freq_means'],
+                       'f17': df_fd['haccel_freq_maxs'],
+                       'f18': df_fd['vaccel_freq_maxs'],
+                       'f19': df_fd['haccel_freq_stds'],
+                       'f20': df_fd['vaccel_freq_stds'],
+                       'f21': df_fd['haccel_freq_vars'],
+                       'f22': df_fd['vaccel_freq_vars'],
+                       'f23': df_fd['haccel_freq_kurts'],
+                       'f24': df_fd['vaccel_freq_kurts'],
+                       'f25': df_fd['haccel_freq_skews'],
+                       'f26': df_fd['vaccel_freq_skews'],
+
+                       'f27': df_tfd['haccel_waveletcoeff1_means'],
+                       'f28': df_tfd['vaccel_waveletcoeff1_means'],
+                       'f29': df_tfd['haccel_waveletcoeff1_maxs'],
+                       'f30': df_tfd['vaccel_waveletcoeff1_maxs'],
+                       'f31': df_tfd['haccel_waveletcoeff1_mins'],
+                       'f32': df_tfd['vaccel_waveletcoeff1_mins'],
+                       'f33': df_tfd['haccel_waveletcoeff1_vars'],
+                       'f34': df_tfd['vaccel_waveletcoeff1_vars'],
+                       'f35': df_tfd['haccel_waveletcoeff1_kurts'],
+                       'f36': df_tfd['vaccel_waveletcoeff1_kurts'],
+                       'f37': df_tfd['haccel_waveletcoeff1_skews'],
+                       'f38': df_tfd['vaccel_waveletcoeff1_skews'],
+
+                       'f39': df_tfd['haccel_waveletcoeff2_means'],
+                       'f40': df_tfd['vaccel_waveletcoeff2_means'],
+                       'f41': df_tfd['haccel_waveletcoeff2_maxs'],
+                       'f42': df_tfd['vaccel_waveletcoeff2_maxs'],
+                       'f43': df_tfd['haccel_waveletcoeff2_mins'],
+                       'f44': df_tfd['vaccel_waveletcoeff2_mins'],
+                       'f45': df_tfd['haccel_waveletcoeff2_vars'],
+                       'f46': df_tfd['vaccel_waveletcoeff2_vars'],
+                       'f47': df_tfd['haccel_waveletcoeff2_kurts'],
+                       'f48': df_tfd['vaccel_waveletcoeff2_kurts'],
+                       'f49': df_tfd['haccel_waveletcoeff2_skews'],
+                       'f50': df_tfd['vaccel_waveletcoeff2_skews'],
+                       
+                       'f51': df_tfd['haccel_waveletcoeff3_means'],
+                       'f52': df_tfd['vaccel_waveletcoeff3_means'],
+                       'f53': df_tfd['haccel_waveletcoeff3_maxs'],
+                       'f54': df_tfd['vaccel_waveletcoeff3_maxs'],
+                       'f55': df_tfd['haccel_waveletcoeff3_mins'],
+                       'f56': df_tfd['vaccel_waveletcoeff3_mins'],
+                       'f57': df_tfd['haccel_waveletcoeff3_vars'],
+                       'f58': df_tfd['vaccel_waveletcoeff3_vars'],
+                       'f59': df_tfd['haccel_waveletcoeff3_kurts'],
+                       'f60': df_tfd['vaccel_waveletcoeff3_kurts'],
+                       'f61': df_tfd['haccel_waveletcoeff3_skews'],
+                       'f62': df_tfd['vaccel_waveletcoeff3_skews'],
+
+                       'RUL': (len(df_td)-1) - df_td['time']/10})
+    df['RUL'] = np.where(df['RUL'] >= 800, 800, df['RUL'] )
+    # MinMax normalization (from 0 to 1)
+    cols_normalize = df.columns.difference(['RUL','time'])
+    scaler = MinMaxScaler()
+    # scaler = MaxAbsScaler()
+    # scaler = StandardScaler()
+    norm_data = pd.DataFrame(scaler.fit_transform(df[cols_normalize]),columns=cols_normalize, index=df.index)
+    join_data = df[df.columns.difference(cols_normalize)].join(norm_data)
+    df = join_data.reindex(columns = df.columns)
+    df.to_csv('input/'+bearing + '_final_features.csv',  encoding='utf-8',index = None)
+
+
+for bearing in testingbearings:
+    df_td = pd.read_csv('input/'+bearing + '_time_domain_features.csv')
+    df_fd = pd.read_csv('input/'+bearing + '_freq_domain_features.csv')
+    df_tfd = pd.read_csv('input/'+bearing + '_timefreq_domain_features.csv')
+    
+    pos = testingbearings.index(bearing)
+
+    df = pd.DataFrame({'time': df_td['time'],
+                       'f1': df_td['haccel_mean'],
+                       'f2': df_td['vaccel_mean'],
+                       'f3': df_td['haccel_max'],
+                       'f4': df_td['vaccel_max'],
+                       'f5': df_td['haccel_min'],
+                       'f6': df_td['vaccel_min'],
+                       'f7': df_td['haccel_p2p'],
+                       'f8': df_td['vaccel_p2p'],
+                       'f9': df_td['haccel_var'],
+                       'f10': df_td['vaccel_var'],
+                       'f11': df_td['haccel_kurt'],
+                       'f12': df_td['vaccel_kurt'],
+                       'f13': df_td['haccel_skew'],
+                       'f14': df_td['vaccel_skew'],
+
+                       'f15': df_fd['haccel_freq_means'],
+                       'f16': df_fd['vaccel_freq_means'],
+                       'f17': df_fd['haccel_freq_maxs'],
+                       'f18': df_fd['vaccel_freq_maxs'],
+                       'f19': df_fd['haccel_freq_stds'],
+                       'f20': df_fd['vaccel_freq_stds'],
+                       'f21': df_fd['haccel_freq_vars'],
+                       'f22': df_fd['vaccel_freq_vars'],
+                       'f23': df_fd['haccel_freq_kurts'],
+                       'f24': df_fd['vaccel_freq_kurts'],
+                       'f25': df_fd['haccel_freq_skews'],
+                       'f26': df_fd['vaccel_freq_skews'],
+
+                       'f27': df_tfd['haccel_waveletcoeff1_means'],
+                       'f28': df_tfd['vaccel_waveletcoeff1_means'],
+                       'f29': df_tfd['haccel_waveletcoeff1_maxs'],
+                       'f30': df_tfd['vaccel_waveletcoeff1_maxs'],
+                       'f31': df_tfd['haccel_waveletcoeff1_mins'],
+                       'f32': df_tfd['vaccel_waveletcoeff1_mins'],
+                       'f33': df_tfd['haccel_waveletcoeff1_vars'],
+                       'f34': df_tfd['vaccel_waveletcoeff1_vars'],
+                       'f35': df_tfd['haccel_waveletcoeff1_kurts'],
+                       'f36': df_tfd['vaccel_waveletcoeff1_kurts'],
+                       'f37': df_tfd['haccel_waveletcoeff1_skews'],
+                       'f38': df_tfd['vaccel_waveletcoeff1_skews'],
+
+                       'f39': df_tfd['haccel_waveletcoeff2_means'],
+                       'f40': df_tfd['vaccel_waveletcoeff2_means'],
+                       'f41': df_tfd['haccel_waveletcoeff2_maxs'],
+                       'f42': df_tfd['vaccel_waveletcoeff2_maxs'],
+                       'f43': df_tfd['haccel_waveletcoeff2_mins'],
+                       'f44': df_tfd['vaccel_waveletcoeff2_mins'],
+                       'f45': df_tfd['haccel_waveletcoeff2_vars'],
+                       'f46': df_tfd['vaccel_waveletcoeff2_vars'],
+                       'f47': df_tfd['haccel_waveletcoeff2_kurts'],
+                       'f48': df_tfd['vaccel_waveletcoeff2_kurts'],
+                       'f49': df_tfd['haccel_waveletcoeff2_skews'],
+                       'f50': df_tfd['vaccel_waveletcoeff2_skews'],
+                       
+                       'f51': df_tfd['haccel_waveletcoeff3_means'],
+                       'f52': df_tfd['vaccel_waveletcoeff3_means'],
+                       'f53': df_tfd['haccel_waveletcoeff3_maxs'],
+                       'f54': df_tfd['vaccel_waveletcoeff3_maxs'],
+                       'f55': df_tfd['haccel_waveletcoeff3_mins'],
+                       'f56': df_tfd['vaccel_waveletcoeff3_mins'],
+                       'f57': df_tfd['haccel_waveletcoeff3_vars'],
+                       'f58': df_tfd['vaccel_waveletcoeff3_vars'],
+                       'f59': df_tfd['haccel_waveletcoeff3_kurts'],
+                       'f60': df_tfd['vaccel_waveletcoeff3_kurts'],
+                       'f61': df_tfd['haccel_waveletcoeff3_skews'],
+                       'f62': df_tfd['vaccel_waveletcoeff3_skews'],
+
+                       'RUL': finalRUL[pos]+(len(df_td)-1) - df_td['time']/10})
+    # Set RUL label
+    df['RUL'] = np.where(df['RUL'] >= 800, 800, df['RUL'] )
+    # MinMax normalization (from 0 to 1)
+    cols_normalize = df.columns.difference(['RUL','time'])
+    scaler = MinMaxScaler()
+    # scaler = MaxAbsScaler()
+    # scaler = StandardScaler()
+    norm_data = pd.DataFrame(scaler.fit_transform(df[cols_normalize]),columns=cols_normalize, index=df.index)
+    join_data = df[df.columns.difference(cols_normalize)].join(norm_data)
+    df = join_data.reindex(columns = df.columns)
+    df.to_csv('input/'+bearing + '_final_features.csv',  encoding='utf-8',index = None)
+
+
+d11 = pd.read_csv('input/Bearing1_1_final_features.csv')
+d12 = pd.read_csv('input/Bearing1_2_final_features.csv')
+d13 = pd.read_csv('input/Bearing1_3_final_features.csv')
+d14 = pd.read_csv('input/Bearing1_4_final_features.csv')
+d15 = pd.read_csv('input/Bearing1_5_final_features.csv')
+d16 = pd.read_csv('input/Bearing1_6_final_features.csv')
+d17 = pd.read_csv('input/Bearing1_7_final_features.csv')
+d21 = pd.read_csv('input/Bearing2_1_final_features.csv')
+d22 = pd.read_csv('input/Bearing2_2_final_features.csv')
+d23 = pd.read_csv('input/Bearing2_3_final_features.csv')
+d24 = pd.read_csv('input/Bearing2_4_final_features.csv')
+d25 = pd.read_csv('input/Bearing2_5_final_features.csv')
+d26 = pd.read_csv('input/Bearing2_6_final_features.csv')
+d27 = pd.read_csv('input/Bearing2_7_final_features.csv')
+d31 = pd.read_csv('input/Bearing3_1_final_features.csv')
+d32 = pd.read_csv('input/Bearing3_2_final_features.csv')
+d33 = pd.read_csv('input/Bearing3_3_final_features.csv')
+
+#convinient for time windows
+d11['id']=1
+d12['id']=2
+d13['id']=1
+d14['id']=2
+d15['id']=3
+d16['id']=4
+d17['id']=5
+d21['id']=1
+d22['id']=2
+d23['id']=1
+d24['id']=2
+d25['id']=3
+d26['id']=4
+d27['id']=5
+d31['id']=1
+d32['id']=2
+d33['id']=1
+
+#Split the dataset into training sets and testing sets
+L1 = pd.concat ([d11,d12],ignore_index=True)
+L2 = pd.concat ([d21,d22],ignore_index=True)
+L3 = pd.concat ([d31,d32],ignore_index=True)
+T1 = pd.concat ([d13,d14,d15,d16,d17],ignore_index=True)
+T2 = pd.concat ([d23,d24,d25,d26,d27],ignore_index=True)
+T3 = d33
+
+L1.to_csv('input/LearningSet1.csv',  encoding='utf-8',index = None)
+L2.to_csv('input/LearningSet2.csv',  encoding='utf-8',index = None)
+L3.to_csv('input/LearningSet3.csv',  encoding='utf-8',index = None)
+T1.to_csv('input/TestingSet1.csv',  encoding='utf-8',index = None)
+T2.to_csv('input/TestingSet2.csv',  encoding='utf-8',index = None)
+T3.to_csv('input/TestingSet3.csv',  encoding='utf-8',index = None)
---
+++ featuremethod/timedomain.py
@@ -0,0 +1,56 @@
+import pandas as pd
+import os
+import glob
+import matplotlib.pyplot as plt
+import numpy as np
+from numpy.fft import fft, fftfreq
+
+bearings = ['Bearing1_1','Bearing1_2','Bearing1_3','Bearing1_4','Bearing1_5','Bearing1_6','Bearing1_7','Bearing2_1','Bearing2_2','Bearing2_3',
+'Bearing2_4','Bearing2_5','Bearing2_6','Bearing2_7','Bearing3_1','Bearing3_2','Bearing3_3']
+names = ['hour', 'minute', 'second', 'micro', 'haccel', 'vaccel']
+
+for bearing in bearings:
+    os.chdir('Data_set/' + bearing)
+    i = 0
+    haccel_means, vaccel_means, haccel_maxs, vaccel_maxs, haccel_mins, vaccel_mins, haccel_p2ps, vaccel_p2ps, haccel_vars, vaccel_vars, haccel_kurts, vaccel_kurts, haccel_skews, vaccel_skews = [[] for _ in range(14)]
+    for fname in glob.glob('acc*.csv'):
+        # if i % 10 != 0:
+        #     i += 1
+        #     continue
+        df = pd.read_csv(fname, names=names)
+        haccel_means.append(df['haccel'].mean())
+        vaccel_means.append(df['vaccel'].mean())
+        haccel_maxs.append(df['haccel'].max())
+        vaccel_maxs.append(df['vaccel'].max())
+        haccel_mins.append(df['haccel'].min())
+        vaccel_mins.append(df['vaccel'].min())
+        haccel_p2ps.append(df['haccel'].max()-df['haccel'].min())
+        vaccel_p2ps.append(df['vaccel'].max()-df['vaccel'].min())       
+        haccel_vars.append(df['haccel'].var())
+        vaccel_vars.append(df['vaccel'].var())
+        haccel_kurts.append(df['haccel'].kurt())
+        vaccel_kurts.append(df['vaccel'].kurt())
+        haccel_skews.append(df['haccel'].skew())
+        vaccel_skews.append(df['vaccel'].skew())
+        i += 1
+
+    times = [10*_ for _ in range(len(haccel_means))]
+
+    df = pd.DataFrame({'time': times,
+                       'haccel_mean': haccel_means,
+                       'vaccel_mean': vaccel_means,
+                       'haccel_max': haccel_maxs,
+                       'vaccel_max': vaccel_maxs,
+                       'haccel_min': haccel_mins,
+                       'vaccel_min': vaccel_mins,
+                       'haccel_p2p': haccel_p2ps,
+                       'vaccel_p2p': vaccel_p2ps,
+                       'haccel_var': haccel_vars,
+                       'vaccel_var': vaccel_vars,
+                       'haccel_kurt': haccel_kurts,
+                       'vaccel_kurt': vaccel_kurts,
+                       'haccel_skew': haccel_skews,
+                       'vaccel_skew': vaccel_skews})
+
+    os.chdir('../..')  # save csv to project directory
+    df.to_csv('input/'+bearing + '_time_domain_features.csv',  encoding='utf-8',index = None)
