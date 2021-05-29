14feffb736e8e915738449d62ecbde826d09f020
tlzm <50614481+tlzm@users.noreply.github.com>
2021-05-29 13:50:08 +0800

    Update STFTdataprocess.py

--- STFTmethod/STFTdataprocess.py
+++ STFTmethod/STFTdataprocess.py
@@ -8,9 +8,8 @@ import math
 from numpy.fft import fft, fftfreq
 import scipy.signal as signal
 
-bearings = ['Bearing1_1']
-#'Bearing1_2','Bearing1_3','Bearing1_4','Bearing1_5','Bearing1_6','Bearing1_7','Bearing2_1','Bearing2_2','Bearing2_3',
-#'Bearing2_4','Bearing2_5','Bearing2_6','Bearing2_7','Bearing3_1','Bearing3_2','Bearing3_3']
+bearings = ['Bearing1_1','Bearing1_2','Bearing1_3','Bearing1_4','Bearing1_5','Bearing1_6','Bearing1_7','Bearing2_1','Bearing2_2','Bearing2_3',
+'Bearing2_4','Bearing2_5','Bearing2_6','Bearing2_7','Bearing3_1','Bearing3_2','Bearing3_3']
 names = ['hour', 'minute', 'second', 'micro', 'haccel', 'vaccel']
 
 #t1,t2,t3,t4,t5,t6,t7 = [[] for _ in range(16)]
@@ -22,8 +21,11 @@ times=[]
 #print(t21)
 position=[]
 
+finalRUL = [0,0,573,290,161,146,757,0,0,753,139,309,129,58,0,0,82]
+
 for bearing in bearings:
     os.chdir('Data_set/' + bearing)
+    pos = bearings.index(bearing)
     i = 0
     haccel_means, vaccel_means, haccel_maxs, vaccel_maxs, haccel_mins, vaccel_mins, haccel_p2ps, vaccel_p2ps, haccel_vars, vaccel_vars, haccel_kurts, vaccel_kurts, haccel_skews, vaccel_skews,haccel_rmss, vaccel_rmss = [[] for _ in range(16)]
     for fname in glob.glob('acc*.csv'):
@@ -65,8 +67,10 @@ for bearing in bearings:
 
     #times = [10*_ for _ in range(len(haccel_means))]
 
-    df = pd.DataFrame({'time': times,
-                       'postion':position,
+    n_time = np.array(times).max() 
+
+    df = pd.DataFrame({'id': times,
+#                       'postion':position,
                        't1':t1,
                        't2':t2,
                        't3':t3,
@@ -87,7 +91,75 @@ for bearing in bearings:
                        't18':t18,
                        't19':t19,
                        't20':t20,
-                       't21':t21})
+                       't21':t21,
+                       'RUL': finalRUL[pos] + n_time - times})
+    df['RUL'] = np.where(df['RUL'] >= 1001, 1001, df['RUL'] )
+    
+#    df=df[~(df['RUL'].isin([801]))]
 
     os.chdir('../..')  # save csv to project directory
     df.to_csv('input/'+bearing + '_STFT.csv',  encoding='utf-8',index = None)
+
+
+d11 = pd.read_csv('input/Bearing1_1_STFT.csv')
+d12 = pd.read_csv('input/Bearing1_2_STFT.csv')
+d13 = pd.read_csv('input/Bearing1_3_STFT.csv')
+d14 = pd.read_csv('input/Bearing1_4_STFT.csv')
+d15 = pd.read_csv('input/Bearing1_5_STFT.csv')
+d16 = pd.read_csv('input/Bearing1_6_STFT.csv')
+d17 = pd.read_csv('input/Bearing1_7_STFT.csv')
+d21 = pd.read_csv('input/Bearing2_1_STFT.csv')
+d22 = pd.read_csv('input/Bearing2_2_STFT.csv')
+d23 = pd.read_csv('input/Bearing2_3_STFT.csv')
+d24 = pd.read_csv('input/Bearing2_4_STFT.csv')
+d25 = pd.read_csv('input/Bearing2_5_STFT.csv')
+d26 = pd.read_csv('input/Bearing2_6_STFT.csv')
+d27 = pd.read_csv('input/Bearing2_7_STFT.csv')
+d31 = pd.read_csv('input/Bearing3_1_STFT.csv')
+d32 = pd.read_csv('input/Bearing3_2_STFT.csv')
+d33 = pd.read_csv('input/Bearing3_3_STFT.csv')
+
+d12['id']=d12['id']+2803
+d14['id']=d14['id']+1802
+d15['id']=d15['id']+2941
+d16['id']=d16['id']+5243
+d17['id']=d17['id']+7545
+d22['id']=d22['id']+911
+d24['id']=d24['id']+1202
+d25['id']=d25['id']+1814
+d26['id']=d26['id']+3816
+d27['id']=d27['id']+4388
+d32['id']=d32['id']+515
+
+#bearing No.
+d11['bearing']=1
+d12['bearing']=2
+d13['bearing']=1
+d14['bearing']=2
+d15['bearing']=3
+d16['bearing']=4
+d17['bearing']=5
+d21['bearing']=1
+d22['bearing']=2
+d23['bearing']=1
+d24['bearing']=2
+d25['bearing']=3
+d26['bearing']=4
+d27['bearing']=5
+d31['bearing']=1
+d32['bearing']=2
+d33['bearing']=1
+
+L1 = pd.concat ([d11,d12],ignore_index=True)
+L2 = pd.concat ([d21,d22],ignore_index=True)
+L3 = pd.concat ([d31,d32],ignore_index=True)
+T1 = pd.concat ([d13,d14,d15,d16,d17],ignore_index=True)
+T2 = pd.concat ([d23,d24,d25,d26,d27],ignore_index=True)
+T3 = d33
+
+L1.to_csv('input/LearningSet1.csv',  encoding='utf-8',index = None)
+L2.to_csv('input/LearningSet2.csv',  encoding='utf-8',index = None)
+L3.to_csv('input/LearningSet3.csv',  encoding='utf-8',index = None)
+T1.to_csv('input/TestingSet1.csv',  encoding='utf-8',index = None)
+T2.to_csv('input/TestingSet2.csv',  encoding='utf-8',index = None)
+T3.to_csv('input/TestingSet3.csv',  encoding='utf-8',index = None)
