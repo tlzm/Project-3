e6a41b024dff44a4c5eda56cd133987fe64253ec
tlzm <50614481+tlzm@users.noreply.github.com>
2021-07-21 18:07:10 +0800

    Update trainingRCNN.py

--- lifelong learning/trainingRCNN.py
+++ lifelong learning/trainingRCNN.py
@@ -20,75 +20,48 @@ train_data = pd.read_csv('input/LearningSet1.csv')
 #train_data = pd.read_csv('input/LearningSet3.csv')
 
 # time windows
-sequence_length = 2556
+sequence_length = 2541
 
 n_time = int(train_data['id'].unique().max())
 
-def reshapeFeaturestrain(id_df, seq_length, Feature):
+def reshapeFeatures(id_df, seq_length, Feature):
     """
     Only sequences that meet the window-length are considered, no padding is used. This means for testing
     we need to drop those which are below the window-length.
     """
     data_matrix = id_df[Feature].values
-    num_elements = data_matrix.shape[0]
-    for start, stop in zip(range(0, num_elements-seq_length), range(seq_length, num_elements)):
+    num_elements = data_matrix.shape[0] 
+    for start, stop in zip(range(0, num_elements-seq_length+1), range(seq_length, num_elements+1)):
         yield data_matrix[start:stop,:]
-
-def reshapeFeaturesval(id_df, seq_length, Feature):
-    """
-    Only sequences that meet the window-length are considered, no padding is used. This means for testing
-    we need to drop those which are below the window-length.
-    """
-    data_matrix = id_df[Feature].values
-    num_elements = data_matrix.shape[0]
-    for start, stop in zip(range(num_elements-seq_length, num_elements-seq_length+1), range( num_elements, num_elements+1)):
-        yield data_matrix[start:stop,:]
-
 
 # pick the feature columns 
 feature_col = ['haccel','vaccel']
 #print(feature_col)
 
 # generator for the sequences
-fea_gen = (list(reshapeFeaturestrain(train_data[train_data['id']==id], sequence_length, feature_col)) for id in range(1, n_time + 1))
+fea_gen = (list(reshapeFeatures(train_data[train_data['id']==id], sequence_length, feature_col)) for id in range(1, n_time + 1))
 
 # generate sequences and convert to numpy array
 fea_array = np.concatenate(list(fea_gen)).astype(np.float32)
 
-
-feaval_gen = (list(reshapeFeaturesval(train_data[train_data['id']==id], sequence_length, feature_col)) for id in range(1, n_time + 1))
-feaval_array = np.concatenate(list(feaval_gen)).astype(np.float32)
-
-
 print("The data set has now shape: {} entries, {} time windows and {} features.".format(fea_array.shape[0],fea_array.shape[1],fea_array.shape[2]))
-print("The val data set has now shape: {} entries, {} time windows and {} features.".format(feaval_array.shape[0],feaval_array.shape[1],feaval_array.shape[2]))
-# function to generate label
-def reshapeLabeltrain(id_df, seq_length=sequence_length, label=['RUL']):
-    """ Only sequences that meet the window-length are considered, no padding is used. This means for testing
-    we need to drop those which are below the window-length."""
-    data_matrix = id_df[label].values
-    num_elements = data_matrix.shape[0]
-    return data_matrix[seq_length-1: num_elements-1, :]
 
-def reshapeLabelval(id_df, seq_length=sequence_length, label=['RUL']):
+# function to generate label
+def reshapeLabel(id_df, seq_length=sequence_length, label=['RUL']):
     """ Only sequences that meet the window-length are considered, no padding is used. This means for testing
     we need to drop those which are below the window-length."""
     data_matrix = id_df[label].values
     num_elements = data_matrix.shape[0]
-    return data_matrix[num_elements-1: num_elements, :]
+    return data_matrix[seq_length-1: num_elements, :]
 
 # generate labels
-label_gen = [reshapeLabeltrain(train_data[train_data['id']==id]) for id in range(1, n_time + 1)]
+label_gen = [reshapeLabel(train_data[train_data['id']==id]) for id in range(1, n_time + 1)]
 
 label_array = np.concatenate(label_gen).astype(np.float32)
 
-labelval_gen = [reshapeLabelval(train_data[train_data['id']==id]) for id in range(1, n_time + 1)]
-
-labelval_array = np.concatenate(labelval_gen).astype(np.float32)
-
-
 print(label_array.shape)
-print(labelval_array.shape)
+
+
 # MODEL
 
 def root_mean_squared_error(y_true, y_pred):
